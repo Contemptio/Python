de72f55f2aa62570059cc0f6cd53d96931fc7a3c
Contemptio <zmk.zk.dev@gmail.com>
2020-01-24 13:43:06 +0100

    Adds options to macro calculator.

--- mat.py
+++ mat.py
@@ -4,32 +4,21 @@
     @date   2020-01-24
 """
 
-import sys
+import getopt, sys
 
 KG_PER_LB = 0.4535924
 LB_PER_KG = 2.204623
+DEFAULT_KCAL = int(15 * LB_PER_KG)
+DEFAULT_RATIO = 0.75
+DEFAULT_UNIT = "kg"
 KCAL_CARBS = 4
 KCAL_FAT = 4
 KCAL_FIBER = 2
 KCAL_PROTEIN = 4
 RATIO = 0.75
 
-def toNumber(string) :
-    try :
-        return float(string)
-    except Exception as e :
-        error("Error, %s has to be a floating point number: %s" % (string, str(e)))
-
-def convert(number, arg) :
-    if arg[0] == "k" or arg == "kg" :
-        return number
-    if arg[0] == "l" or arg == "lb" :
-        return KG_PER_LB * number
-
-    error("Invalid weight type %s." % arg)
-
-def macros(weight, kcalPerKg) :
-    calories = int(kcalPerKg * weight * RATIO)
+def macros(weight, opts) :
+    calories = int(toInt(opts["kcal"]) * weight * toFloat(opts["ratio"]))
     fat = weight // 2
     fiber = calories * 0.014
     protein = weight
@@ -43,26 +32,60 @@ def macros(weight, kcalPerKg) :
         "Fiber" : int(fiber)
     }
 
+def getOpts(argv) :
+    myopts, args = getopt.getopt(argv[2:], "k:r:u:")
+    result = {
+        "kcal" : DEFAULT_KCAL,
+        "ratio" : DEFAULT_RATIO,
+        "unit" : DEFAULT_UNIT
+    }
+
+    for opt, arg in myopts :
+        key = ""
+
+        if opt == "-k" :
+            key = "kcal"
+        elif opt == "-r" :
+            key = "ratio"
+        elif opt == "-u" :
+            key = "unit"
+
+        result[key] = arg
+
+    return result
+
+def convertWeight(weight, unit) :
+    if unit == "kg" :
+        return weight
+    if unit != "lb" :
+        error("Invalid unit: %s." % unit)
+
+    return weight * KG_PER_LB
+
+def toFloat(number) :
+    try :
+        return float(number)
+    except Exception as e :
+        error("Expected a floating number, got " + str(number) + ": " + str(e))
+
+def toInt(number) :
+    try :
+        return int(number)
+    except Exception as e :
+        error("Expected a floating number, got " + str(number) + ": " + str(e))
+
 def error(message) :
     print(message)
     sys.exit(1)
 
 if __name__ == "__main__" :
-    args = sys.argv
-    numArgs = len(args)
+    numArgs = len(sys.argv)
 
-    weight = 0
+    if numArgs <= 1 :
+        error("Usage: py LexicographicPermutations.py <weight> [<opts>]")
 
-    # TODO: Convert to input arg.
-    kcalPerKg = int(15 * 2.204623)
-
-    if numArgs <= 1 or numArgs > 3 :
-        error("Usage: py LexicographicPermutations.py <weight> [kg || lb]")
-    if numArgs >= 2 :
-        weight = toNumber(args[1])
-    if numArgs == 3 :
-        weight = convert(weight, toNumber(args[2]))
-
-    macros = macros(weight, kcalPerKg)
+    opts = getOpts(sys.argv)
+    weight = toFloat(sys.argv[1])
+    macros = macros(convertWeight(weight, opts["unit"]), opts)
 
     print(macros)
